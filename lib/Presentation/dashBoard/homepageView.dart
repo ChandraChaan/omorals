b2469ff4d6d833822eed798ff740f114095b8fa6
LOKESHWARAN1 <mlokesh672000@gmail.com>
2022-06-30 23:31:44 +0530

    Adding vifdeo ui templet

--- lib/Presentation/dashBoard/homepageView.dart
+++ lib/Presentation/dashBoard/homepageView.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:omorals/Config/app_pages.dart';
 import 'package:omorals/Presentation/dashBoard/slideShowView.dart';
+import 'package:omorals/utils/constant.dart';
 
 class HomePageView extends StatefulWidget {
   @override
@@ -18,21 +19,101 @@ class _HomePageViewState extends State<HomePageView> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Container(
+              height: 30,
+              child: Row(
+                mainAxisAlignment:MainAxisAlignment.end,
+                children:[
+                  Center(
+                    child: GetStorage().read('userName') == null
+                ? TextButton(
+                      onPressed: () {
+                        Get.toNamed(Routes.login);
+                      },
+                      child: Text('Login'))
+                : Container(),
+                  )
+                ]
+              ),
+            ),
             SideOffer(),
             SizedBox(
               height: 50,
             ),
-            Text(
-              'Well Come ' + (GetStorage().read('userName') ?? ''),
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            GetStorage().read('userName') == null
-                ? TextButton(
-                    onPressed: () {
-                      Get.toNamed(Routes.login);
-                    },
-                    child: Text('Login'))
-                : Container()
+            Container(
+                height: getHeight(context),
+                child: ListView.builder(
+                  itemCount: 3,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                          height: MediaQuery.of(context).size.height / 2,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.red,
+                              ),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(20))),
+                          child: Center(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  "Title",
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Enter the Title',
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  height:
+                                      MediaQuery.of(context).size.height / 5,
+                                  color: Colors.green,
+                                  child: const Center(
+                                    child: Text("Video"),
+                                  ),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 8.0),
+                                child: Text(
+                                  "Description",
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Enter the description',
+                                  ),
+                                ),
+                              )
+                            ],
+                          ))),
+                    );
+                  },
+                ),
+              ),
           ],
         ),
       ),
